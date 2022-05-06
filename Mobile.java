a3ad80f07e5f0c73361d969e3e56db3212e7f0d6
Madhusudannatarajan <71771441+Madhusudannatarajan@users.noreply.github.com>
2022-05-06 10:09:42 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,25 @@
+package selinum;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String MobileName="Iphone";
+		System.out.println("MobileName:" + MobileName);
+		char firstLetter='I';
+		System.out.println("firstLetter:" +firstLetter);
+		short ramsize=6;
+		int memory=32;
+		System.out.println("memory:" +memory);
+		boolean isDamaged=true;
+		System.out.println("isDamaged:" +isDamaged);
+		double price=40000.000;
+		System.out.println("price:" +price);
+		long ipAddress=23543421;
+		System.out.println("ipAddress:" +ipAddress);
+		float frequencyRange=2.54f;
+		System.out.println("frequencyRange:" +frequencyRange);
+		
+		
+	}
+
+}
