3a3b548a772661affb12c6825d562a3d901ce009
Madhusudannatarajan <71771441+Madhusudannatarajan@users.noreply.github.com>
2022-05-11 10:20:22 +0530

    Add files via upload

---
+++ day3/FacebookLogin.java
@@ -0,0 +1,21 @@
+package week2.day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.SendKeysAction;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FacebookLogin {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().fullscreen();
+		
+		driver.findElement(By.id("email")).sendKeys("madhusudannatarajan07@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("maddyrb1128");
+	    driver.findElement(By.name("login")).click();
+	}
+
+}
